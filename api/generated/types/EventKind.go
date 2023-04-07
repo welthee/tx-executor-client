e1fc77a883c83bf874572e3fd57fd7b59b683449
ci <ci@welthee.com>
2023-04-07 10:00:28 +0000

    chore(models): update generated models - 0.1.0-79-g4ec72d8

--- api/generated/types/EventKind.go
+++ api/generated/types/EventKind.go
@@ -9,9 +9,6 @@ const (
   EventKindInterrogated
   EventKindSpeededup
   EventKindCompleted
-  EventKindAccountCreated
-  EventKindAccountDetailed
-  EventKindAccountDisabled
 )
 
 // Value returns the value of the enum.
@@ -22,13 +19,10 @@ func (op EventKind) Value() any {
 	return EventKindValues[op]
 }
 
-var EventKindValues = []any{"submitted","interrogated","speededup","completed","accountCreated","accountDetailed","accountDisabled"}
+var EventKindValues = []any{"submitted","interrogated","speededup","completed"}
 var ValuesToEventKind = map[any]EventKind{
   EventKindValues[EventKindSubmitted]: EventKindSubmitted,
   EventKindValues[EventKindInterrogated]: EventKindInterrogated,
   EventKindValues[EventKindSpeededup]: EventKindSpeededup,
   EventKindValues[EventKindCompleted]: EventKindCompleted,
-  EventKindValues[EventKindAccountCreated]: EventKindAccountCreated,
-  EventKindValues[EventKindAccountDetailed]: EventKindAccountDetailed,
-  EventKindValues[EventKindAccountDisabled]: EventKindAccountDisabled,
 }
